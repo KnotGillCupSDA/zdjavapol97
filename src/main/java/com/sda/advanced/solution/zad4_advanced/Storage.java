81837c6671d14a1e1df731c6640d72ea06ff6b57
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-09-25 14:34:14 +0200

    zad4 advanced

--- src/main/java/com/sda/advanced/solution/zad4_advanced/Storage.java
+++ src/main/java/com/sda/advanced/solution/zad4_advanced/Storage.java
@@ -1,5 +1,6 @@
 package com.sda.advanced.solution.zad4_advanced;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,18 +8,27 @@ import java.util.stream.Collectors;
 
 public class Storage {
 
-	private final Map<String, String> map;
+	private final Map<String, List<String>> map;
 
 	public Storage() {
 		map = new HashMap<>();
 	}
 
 	void addToStorage(String key, String value) {
-		map.put(key, value);
+		if (map.containsKey(key)) {
+			//if already contains the key, add another element to the list
+			List<String> strings = map.get(key);
+			strings.add(value);
+		} else {
+			//if does not contain the key, create new list with one element and put that into map
+			List<String> strings = new ArrayList<>();
+			strings.add(value);
+			map.put(key, strings);
+		}
 	}
 
 	void printValues(String key) {
-		if(map.containsKey(key)) {
+		if (map.containsKey(key)) {
 			System.out.println(map.get(key));
 		} else {
 			System.out.println("Nie znaleziono pozycji");
