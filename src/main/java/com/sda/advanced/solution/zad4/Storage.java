d9ca5cb2774a045d0a11ed4ab277e66b7cf911d2
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-09-25 13:58:42 +0200

    zad4 done without multiple values under single key

--- src/main/java/com/sda/advanced/solution/zad4/Storage.java
+++ src/main/java/com/sda/advanced/solution/zad4/Storage.java
@@ -5,23 +5,42 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Storage {
 
-	private final Map<String, String> storage;
+	private final Map<String, String> map;
 
 	public Storage() {
-		storage = new HashMap<>();
+		map = new HashMap<>();
 	}
 
 	void addToStorage(String key, String value) {
+		map.put(key, value);
 	}
 
 	void printValues(String key) {
-
+		if(map.containsKey(key)) {
+			System.out.println(map.get(key));
+		} else {
+			System.out.println("Nie znaleziono pozycji");
+		}
 	}
 
 	List<String> findKeysByValue(String value) {
-		return Collections.emptyList();
+
+		/*List<String> foundKeys = new ArrayList<>();
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			if(entry.getValue().equals(value)) {
+				foundKeys.add(entry.getKey());
+			}
+		}
+		return foundKeys;*/
+
+		return map.entrySet()
+				.stream()
+				.filter(entry -> entry.getValue().equals(value))
+				.map(entry -> entry.getKey())
+				.collect(Collectors.toList());
 	}
 }
