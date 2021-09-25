bc652355dbc9db079af3185b79cd685cc255b10d
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-09-25 11:52:20 +0200

    zad4 draft

---
+++ src/main/java/com/sda/advanced/solution/zad4/Main.java
@@ -0,0 +1,21 @@
+package com.sda.advanced.solution.zad4;
+
+public class Main {
+	public static void main(String[] args) {
+		Storage storage = new Storage();
+
+		storage.addToStorage("klucz1", "wartosc1");
+		storage.addToStorage("klucz1", "wartosc2");
+		storage.addToStorage("klucz2", "wartosc1");
+		storage.addToStorage("klucz2", "wartosc3");
+		storage.addToStorage("klucz3", "wartosc1");
+
+		System.out.println("printValues");
+		storage.printValues("klucz1");
+		storage.printValues("klucz2");
+		storage.printValues("klucz3");
+
+		System.out.println("findKeysByValue");
+		System.out.println(storage.findKeysByValue("wartosc1"));
+	}
+}
---
+++ src/main/java/com/sda/advanced/solution/zad4/Storage.java
@@ -0,0 +1,19 @@
+package com.sda.advanced.solution.zad4;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Storage {
+
+	void addToStorage(String key, String value) {
+
+	}
+
+	void printValues(String key) {
+
+	}
+
+	List<String> findKeysByValue(String value) {
+		return Collections.emptyList();
+	}
+}
