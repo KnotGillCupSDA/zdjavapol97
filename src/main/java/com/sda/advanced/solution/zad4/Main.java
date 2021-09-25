7b03324ffe701c3107ac2ab5534d462b6add726b
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-09-25 13:15:45 +0200

    zad4 draft2

--- src/main/java/com/sda/advanced/solution/zad4/Main.java
+++ src/main/java/com/sda/advanced/solution/zad4/Main.java
@@ -4,8 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		Storage storage = new Storage();
 
-		storage.addToStorage("klucz1", "wartosc1");
-		storage.addToStorage("klucz1", "wartosc2");
+		storage.addToStorage("klucz1", "wartosc1"); // klucz1: [wartosc1]
+		storage.addToStorage("klucz1", "wartosc2"); // klucz1: [wartosc1, wartosc2]
+
 		storage.addToStorage("klucz2", "wartosc1");
 		storage.addToStorage("klucz2", "wartosc3");
 		storage.addToStorage("klucz3", "wartosc1");
--- src/main/java/com/sda/advanced/solution/zad4/Storage.java
+++ src/main/java/com/sda/advanced/solution/zad4/Storage.java
@@ -1,12 +1,20 @@
 package com.sda.advanced.solution.zad4;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Storage {
 
-	void addToStorage(String key, String value) {
+	private final Map<String, String> storage;
 
+	public Storage() {
+		storage = new HashMap<>();
+	}
+
+	void addToStorage(String key, String value) {
 	}
 
 	void printValues(String key) {
