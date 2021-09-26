8adfe1eed61de88cf0a4bca0cb95b10d71a6e4f1
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-09-26 11:39:04 +0200

    generics playground zad26 draft

---
+++ src/main/java/com/sda/advanced/solution/generics/PlayGround.java
@@ -0,0 +1,22 @@
+package com.sda.advanced.solution.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayGround {
+
+	public static void main(String[] args) {
+
+		//w starej javie nie było generyków
+		List strings = new ArrayList();
+
+		//Możemy zapakować cokolwiek
+		strings.add(1);
+		strings.add("123456789");
+
+		//na etapie kompilacji nie wykryjemy błędu
+		String o = (String) strings.get(0);
+		System.out.println(o.substring(2));
+	}
+
+}
